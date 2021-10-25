78d5d3b68e43b97151544454bef80f10556bf866
Kyle Fleming <kyleathollfleming@gmail.com>
2021-10-25 16:28:36 +0200

    Second commit, began creating models for User and Image and added folders for Repository, models, and controllers

---
+++ Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2_Images.Models
+{
+    public class Image
+    {
+        public string FileName { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int Uploader { get; set; }
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2_Images.Models
+{
+    public class User
+    {
+        public string email { get; set; }
+        public int userId { get; set; } 
+    }
+}
